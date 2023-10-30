613431e14afd67cd444e16b1835de4319b1c61c8
AzharM <Gr33nMayhem@gmail.com>
2023-10-30 14:17:58 -0400

    minor bug fix

--- generated_files/InventoryRecord_pb2_grpc.py
+++ generated_files/InventoryRecord_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import InventoryRecord_pb2 as InventoryRecord__pb2
+from . import InventoryRecord_pb2 as InventoryRecord__pb2
 
 
 class InventoryRecordServiceStub(object):
